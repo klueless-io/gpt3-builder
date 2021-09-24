859df1c6b2fb376ae26c7d238f505f5d3e1ccf50
David Cruwys <david@ideasmen.com.au>
2021-09-24 13:41:20 +1000

    first commit

---
+++ lib/gpt3/builder.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "builder/version"
+
+module Gpt3
+  module Builder
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/gpt3/builder_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Gpt3::Builder do
+  it "has a version number" do
+    expect(Gpt3::Builder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
